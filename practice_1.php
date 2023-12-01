b8b7d909a1eac3e381cf82b33d79cac0915c2153
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-12-01 05:24:26 +0000

    [PHP]関数(strlen/count/rand)

--- practice_1.php
+++ practice_1.php
@@ -210,3 +210,24 @@
     echo $key.'は'.$value.'点です。';
   }
 ?>
+
+<?php
+
+  $str = 'progate';
+
+  // strlenを用いて$strの長さをechoしてください
+  echo strlen($str);
+  
+  echo '<br>';
+  
+  $array = array('HTML', 'CSS', 'PHP');
+
+  // countを用いて$arrayの要素数をechoしてください
+  echo count($array);
+  
+  echo '<br>';
+  
+  // randを用いて10から15までのランダムな数字をechoしてください
+  echo rand(10, 15);
+  
+?>
