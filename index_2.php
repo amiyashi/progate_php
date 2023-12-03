01850130497c79f00be68747cd1ae17cb9d92451
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-12-03 07:53:33 +0000

    [PHP]continue文で抜け出す

--- index_2.php
+++ index_2.php
@@ -187,3 +187,14 @@
     echo '<br>';
   }
 ?>
+
+<?php
+  // この下にfor文を書いてください
+  for ($i = 1; $i <= 1000; $i++) {
+    if ($i % 3 == 0) {
+      continue;
+    }
+    echo $i;
+    echo '<br>';
+  }
+?>
