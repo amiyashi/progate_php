1022fcef01a7ced1f571c07d8fb9f266b3a72212
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-12-05 09:27:04 +0000

    [PHP]復習

--- test.php
+++ test.php
@@ -95,3 +95,18 @@
   // 引数を(5, 10)としてprintRectangleAreaを呼び出してください
   printRectangleArea(5, 10);
 ?>
+
+<?php
+
+  // 関数getCircleAreaを定義してください
+  function getCircleArea($radius) {
+    return $radius * $radius * 3;
+  }
+  
+  // 関数getCircleAreaを呼び出して、戻り値を変数$circleAreaに代入してください
+  $circleArea = getCircleArea(5);
+  
+  // $circleAreaをechoしてください
+  echo $circleArea;
+  
+?>
