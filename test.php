dc7e229cb7245c0dcc4a4db712c117d6ad8831f3
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-12-05 09:12:15 +0000

    [PHP]復習

--- test.php
+++ test.php
@@ -67,3 +67,14 @@
   }
    
 ?>
+
+<?php
+
+  $scores = array('数学' => 70, '英語' => 90, '国語' => 80);
+
+  // この下にforeach文を書いてください
+  foreach ($scores as $key => $value) {
+    echo $key.'は'.$value.'点です。';
+  }
+  
+?>
