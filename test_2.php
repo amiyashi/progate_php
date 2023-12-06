52f79933ae6ef6c436081da549bcea872e735ce7
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-12-06 05:04:15 +0000

    [PHP]復習

--- test_2.php
+++ test_2.php
@@ -20,5 +20,40 @@
     echo $i;
     echo '<br>';
   }
-    
+  // この下にfor文を書いてください
+  for ($i = 1; $i <= 1000; $i++) {
+    if ($i % 3 == 0) {
+      continue;
+    }
+    echo $i;
+    echo '<br>';
+  }
+  
+  $scores = array('数学' => 70, '英語' => 90, '国語' => 80);
+
+  // この下にforeach文を書いてください
+  foreach ($scores as $key => $value) {
+    echo $key.'は'.$value.'点です。';
+  }
+  
+  // 関数helloを定義してください
+  function hello() {
+    echo 'Hello, world!';
+  }
+  
+  // 関数helloを呼び出してください
+  hello();
+  
+  // 関数getCircleAreaを定義してください
+  function getCircleArea($radius) {
+    return $radius * $radius * 3;
+  }
+  
+  // 関数getCircleAreaを呼び出して、戻り値を変数$circleAreaに代入してください
+  $circleArea = getCircleArea(5);
+  // $circleAreaをechoしてください
+  echo $circleArea;
+  
+  
+  
 ?>
