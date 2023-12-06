8c73f861d504b9f8017b51d4e1e14719c0c8725e
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-12-06 04:34:41 +0000

    [PHP]復習

--- test_2.php
+++ test_2.php
@@ -1,3 +1,16 @@
 <?php
-// 変数$remainderを定義し、変数$numを3で割った時の余りを代入してください
-$remainder = $num % 3;
+
+  // 変数$remainderを定義し、変数$numを3で割った時の余りを代入してください
+  $remainder = $num % 3;
+  // 変数$iを定義してください
+  $i = 2;
+  // while文を書いてください
+  while ($i <= 100) {
+    if ($i % 2 == 0) {
+      echo $i;
+      echo '<br>';
+    }
+    $i++;
+  }
+    
+?>
