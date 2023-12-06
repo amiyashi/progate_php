8d8caab01589d110cb130559c625fc902e82cdef
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-12-06 13:35:09 +0000

    [PHP]道場

--- dojo_1.php
+++ dojo_1.php
@@ -12,4 +12,57 @@
   }
   echo '<br>';
 }
+
+// この下にコードを書いてください
+$totaPrice = 0;
+foreach ($prices as $price) {
+  $totaPrice += $price;
+}
+echo '合計金額は'.$totaPrice.'円です';
+
+$totalPrice = 0;
+$maxPrice = 0;
+foreach ($prices as $price) {
+  if ($price > $maxPrice) {
+    $maxPrice = $price;
+  }
+}
+echo '合計金額は'.$totalPrice.'円です';
+echo '最高価格は'.$maxPrice.'円です';
+
+// 連想配列
+echo $menu['name'].'は'.$menu['price'].'円です';
+
+foreach ($menus as $menu) {
+  echo $menu['name'].'は'.$menu['price'].'円です';
+  echo '<br>';
+}
+
+$totalPrice = 0;
+foreach ($menus as $menu) {
+  $price = $menu['price'];
+  echo $menu['name'].'は'.$price.'円です';
+  echo '<br>';
+  $totalPrice += $price;
+}
+echo '合計金額は'.$totalPrice.'円です';
+
+$totalPrice = 0;
+$maxPrice = 0;
+$maxPriceMenuName = '';
+foreach ($menus as $menu) {
+  $name = $menu['name'];
+  $price = $menu['price'];
+  echo $name.'は'.$price.'円です';
+  echo '<br>';
+  $totalPrice += $price;
+  if ($price > $maxPrice) {
+    $maxPrice = $price;
+    $maxPriceMenuName = $name;
+  }
+}
+echo '合計金額は'.$totalPrice.'円です';
+echo '<br>';
+echo $maxPriceMenuName.'が最高価格で'.$maxPrice.'円です';
+
 ?>
