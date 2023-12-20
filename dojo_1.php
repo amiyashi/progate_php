efedf31e5c123ee58a4a641bcec20159d15e98b4
amiyashi <138949865+amiyashi@users.noreply.github.com>
2023-12-20 10:46:28 +0000

    [PHP]最高価格を求める

--- dojo_1.php
+++ dojo_1.php
@@ -125,4 +125,22 @@ foreach ($menus as $menu) {
 }
 echo '合計金額は'.$totalPrice.'円です';
 
+$totalPrice = 0;
+$maxPrice = 0;
+$maxPriceMenuName = '';
+foreach ($menus as $menu) {
+  $name = $menu['name'];
+  $price = $menu['price'];
+  echo $name.'は'.$price.'円です';
+  echo '<br>';
+  $totalPrice += $price; 
+  if ($price > $maxPrice) {
+    $maxPrice = $price;
+    $maxPriceMenuName = $name;
+  }
+}
+echo '合計金額は'.$totalPrice.'円です';
+echo '<br>';
+echo $maxPriceMenuName.'が最高価格で'.$maxPrice.'円です';
+
 ?>
